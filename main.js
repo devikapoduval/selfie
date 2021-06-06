8723638e766ddf8b35da193bf64aacd7886c8f23
devikapoduval <77113861+devikapoduval@users.noreply.github.com>
2021-06-06 18:50:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+var SpeechRecognition= window.webkitSpeechRecognition;
+var recognition= new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML=" ";
+    recognition.start();
+}
+
+recognition.onresult= function (event) {
+    console.log(event)
+
+    var content=event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML=content;
+    console.log(content);
+
+    if (content=="take my selfie"){
+        console.log("Take my selfie");
+        
+        speak();
+
+    }
+  
+
+}
+
+function speak () {
+    var synth = window.speechSynthesis;
+    var speak_data="Taking your selfie in 5 seconds";
+    var utter_this=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utter_this);
+
+    Webcam.attach(camera)
+    
+
+    setTimeout(function(){
+    take_snapshot()
+    save()
+    },5000)
+}
+camera= document.getElementById("camera")
+Webcam.set({
+    width:360,
+    height: 250,
+    image_format: "jpeg",
+    jpeg_quality:90
+})
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='selfie_image' src='"+data_uri+"' />"
+    });
+}
+
+function save() {
+   link=document.getElementById("link");
+   image=document.getElementById("selfie_image").src;
+   link.href=image;
+   link.click();
+}
+
+
+
